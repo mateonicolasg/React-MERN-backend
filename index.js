73eb47f1ddfc55438193a3d96ba535e3e98f4681
mateonicolasg <mateonsg12@hotmail.com>
2026-09-15 13:40:29 -0500

    React App ready

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
@@ -21,6 +22,10 @@ app.use(express.json());
 // Rutas
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/events', require('./routes/events'));
+
+app.use('/{*splat}', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
 
 // Escuchar peticiones
 app.listen(process.env.PORT, () => {
